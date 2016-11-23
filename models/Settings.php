344429a54eb1e7e4f1737acd198811801adcc422
mpawley <matthew@rejuvenateproductions.com>
2016-11-23 08:54:23 +0000

    Ensures quality and sharpen can't exceed given scale

--- models/Settings.php
+++ models/Settings.php
@@ -7,6 +7,8 @@ use Model;
  */
 class Settings extends Model
 {
+    use \October\Rain\Database\Traits\Validation;
+
     public $implement = ['System.Behaviors.SettingsModel'];
 
     public $settingsCode = 'toughdeveloper_imageresizer_settings';
@@ -18,5 +20,10 @@ class Settings extends Model
         'default_offset_y' => 'integer',
         'default_quality'  => 'integer',
         'default_sharpen'  => 'integer',
+    ];
+
+    public $rules = [
+        'default_quality'   => 'integer|between:0,100',
+        'default_sharpen'   => 'integer|between:0,100',
     ];
 }
